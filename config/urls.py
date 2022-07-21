c43549cc5e18bc26fdcdefc3b5d1892be1af3dfc
chocot-j <joy01010@naver.com>
2022-07-21 21:31:49 +0900

    modify url and default img

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mnistEx/', include('mnistEx.urls')),
+    path('', include('mnistEx.urls')),
 ]
